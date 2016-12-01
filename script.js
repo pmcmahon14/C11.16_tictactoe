8773a2969fb39534c767b893c65d7c82ca14300f
Patrick McMahon <pmcmahon14@gmail.com>
2016-11-30 19:02:13 -0800

    exploring jquery coding for board grid to determine win

--- script.js
+++ script.js
@@ -54,7 +54,8 @@ var combobig = [[0,5,10,15,20], [1,6,11,16,21], [2,7,12,17,22], [3,8,13,18,23], 
         $(document).ready(function () {
 
             var symbol = ["X", "O"];
-
+            //var grid = [[0,1,2],[3,4,5],[6,7,8]];
+            //var grid = [[0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15,16,17,18,19],[20,21,22,23,24]];
             function Player(id, symbol) {
                 this.id = id;
                 this.symbol = symbol;
