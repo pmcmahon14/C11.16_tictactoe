91776bc9cb5243639c091589f12d62d346f2eb3e
Patrick McMahon <pmcmahon14@gmail.com>
2016-11-30 17:12:43 -0800

    Cleaned up loops

--- script.js
+++ script.js
@@ -7,51 +7,57 @@
 /*tag each cell with a score value*/
 /* add to board setup*/
 
-var cell;
-var score;
-    function scorecell() {
-        for (n=0; n<size-1; n++) {
+
+var set;
+    /*function scorecell() {
+        for (n=0; n<size; n++) {
         cell[0].indicator = indicator;
         cell.click(set);
         row.append(cell);
         squares.push(cell);
         indicator += indicator;
         }
-    }
+    }*/
 
 /* how to tell if a win*/
 var moves;
-var scoretotal = [7, 56, 73, 84, 146, 273, 292, 448];
-    function winsmall() {
-        for (i=0; i<scoretotal.length; i++) {
-            if (playone = scoretotal[i]) {
-                return "X won!";
-            } else if (playtwo = scoretotal[i]) {
-                return "O won!";
-            } else if (moves = 9) {
-                return "It's a tie.";
-            } else {
-            return;
+var combosmall = [[0,3,6], [1,4,7], [2,5,8], [0,4,8], [0,1,2], [3,4,5], [6,7,8], [2,4,6]];
+    win = function three() {
+        for (var i=0; i<combosmall.length; i++) {
+            for (var j=0; j<combosmall[i].length; j++) {
+                var matchsmall = [];
+                matchsmall.push(combosmall[i][j]);
+                if (matchsmall === "XXX") {
+                    console.log("X won!");
+                } else if (matchsmall === "OOO") {
+                    console.log("O won!");
+                } else if (moves = 9) {
+                    console.log("It's a tie.");
+                } else {
+                    return;
+                }
+            }
         }
-        }
-    }
-
+    };
 
 /*if on a 5x5 grid*/
 
-var combos = [[0,5,10,15,20], [1,6,11,16,21], [2,7,12,17,22], [3,8,13,18,23], [4,9,14,19,24], [0,6,12,18,24], [0,1,2,3,4], [5,6,7,8,9], [10,11,12,13,14], [15,16,17,18,19], [20,21,22,23,24], [4,8,12,16,20]];
-    function winbig() {
-        for (i in combos) {
-            var pattern = combos[i];
-            var p = this.board[pattern[0]] + this.board[pattern[5]] + this.board[pattern[10]] + this.board[pattern[15]] + this.board[pattern[20]];
-            if (y === "XXXXX") {
-                return "X won!";
-            } else if (y === "OOOOO") {
-                return "O won!";
-            } else if (moves = 25) {
-                return ("It's a tie.");
-            } else {
-                return;
+var combobig = [[0,5,10,15,20], [1,6,11,16,21], [2,7,12,17,22], [3,8,13,18,23], [4,9,14,19,24], [0,6,12,18,24], [0,1,2,3,4], [5,6,7,8,9], [10,11,12,13,14], [15,16,17,18,19], [20,21,22,23,24], [4,8,12,16,20]];
+    win = function five() {
+        for (var i=0; i<combobig.length; i++) {
+            for (var j=0; j<combobig[i].length; j++) {
+                var matchbig = [];
+                matchbig.push(combobig[i][j]);
+                /*var y = this.board[pattern[combos[]]] + this.board[pattern[5]] + this.board[pattern[10]] + this.board[pattern[15]] + this.board[pattern[20]];*/
+                if (matchbig === "XXXXX") {
+                    console.log("X won!");
+                } else if (matchbig === "OOOOO") {
+                    console.log("O won!");
+                } else if (moves = 25) {
+                    console.log("It's a tie.");
+                } else {
+                    return;
+                }
             }
         }
 
@@ -91,17 +97,23 @@ var combos = [[0,5,10,15,20], [1,6,11,16,21], [2,7,12,17,22], [3,8,13,18,23], [4
 
                 if (this.hasClass('threeXthree')) {
                     console.log("3x3 button was clicked");
+                    /*size = 3;*/
                 }
+                //winsmall goes here?
                 else if (this.hasClass('fiveXfive')) {
                     console.log("5x5 button was clicked");
+                    /*size = 5;*/
                 }
+                //winbig goes here?
                 else {
                     console.log("Reset button was clicked");
                 }
 
             });
 
+
+
         });
-    }
+    };
 
 
